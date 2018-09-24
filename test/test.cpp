c0b96703be91aa086a50c02b0307039b72a49d11
indushekhar <indushekhar05@gmail.com>
2018-09-24 01:41:18 -0400

    Added the unit test for PID in test.cpp and updated CMakelist.txt of test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,31 @@
-#include <gtest/gtest.h>
+/**
+* @file test.cpp
+* @brief Unit tests for PidController class
+* @author Indushekhar Singh
+* @version 1.0
+* @copyright MIT License (c) 2018 Indushekhar Singh
+*/
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include <gtest/gtest.h>
+#include <PidController.hpp>
+
+
+/**
+ * @brief Test for computation of the output velocity
+ */
+
+TEST(PidControllerTest, testCompute) {
+     PidController test_compute(0, 0);
+     double currentVelocity = test_compute.compute(50, 60);
+     EXPECT_NEAR(1.7, currentVelocity, 0.1);
+}
+
+
+/**
+ * @brief Test for Parameterized Constructors
+ */
+
+TEST(PidControllerTest, testParamInit) {
+     PidController test_param_init(0, 3);
+     EXPECT_EQ(3, test_param_init.old_integral);
 }
