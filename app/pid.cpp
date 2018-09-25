023939be02f9a15de3315b6c01ffe21a591a9e7d
bhargav-umd <bhargav@umd.edu>
2018-09-25 12:33:04 -0400

    Pid Class updated

--- app/pid.cpp
+++ app/pid.cpp
@@ -7,9 +7,7 @@
 * @copyright MIT License (c) 2018 Indushekhar Singh
 */
 
-#include <PidController.hpp>
-using std::cout;
-using std::endl;
+#include "../include/PidController.hpp"
 
 /**
 * @brief parametric constructor to initialize the controller
@@ -20,6 +18,8 @@ using std::endl;
 PidController::PidController(double old_error_init, double old_integral_init) {
     // To do : Store the input parameter in old_error
     // and old_integral respectively.
+    old_error = old_error_init;
+    old_integral = old_integral_init;
 }
 
 double PidController::compute(double currentVelocity, double setVelocity) {
@@ -34,8 +34,18 @@ double PidController::compute(double currentVelocity, double setVelocity) {
     // Calculate the proportional output
     // add all the 3 output and store as currentVelocity
     // update old_error to error
-    // return currentVelocity
-  return 1.0;
+    // return currentVelocityi
+    double error = setVelocity - currentVelocity;
+    old_integral += error * dt;
+    double derivative = (error - old_error) / dt;
+    double intergral_out = ki * old_integral;
+    // cout<<"intergral_out"<<intergral_out;
+    double derivative_out = kd * derivative;
+    // cout<<"derivative_out"<<derivative_out;
+    double prop_out = kp * error;
+    // cout<< "prop_out"<< prop_out;
+    currentVelocity = prop_out + intergral_out + derivative_out;
+    // cout<<currentVelocity;
+    old_error = error;
+    return currentVelocity;
 }
-
-
